4a4926273ea76910ce465128e17565cebb3ff737
Jose A <asilis007@hotmail.com>
2019-06-08 09:11:34 -0400

    Started doing BDD

---
+++ src/__tests__/infintieScrolling.test.ts
@@ -0,0 +1,11 @@
+describe('Infinite Scroll Suite', () => {
+  it('Can Render 6 items when toDisplay is 6', () => {
+    const toDisplay = 6;
+    const itemsShown = [];
+    let totalItems = 0;
+    const allItems = [];
+    const itemsNotShown = [];
+
+    expect(itemsShown.length, 6);
+  });
+});
